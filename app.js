eaeef7a3f139dfefcca25a0ccb2eb5ad4d844e4a
Blaise Thomas <blaisegthomas@gmail.com>
2015-04-26 13:08:08 -0700

    adds lists to feelings

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var animateApp = angular.module('animateApp', ['ngRoute', 'ngAnimate']);
+var animateApp = angular.module('animateApp', ['ngRoute', 'ngAnimate', 'firebase']);
 
 animateApp.config(function($routeProvider) {
     $routeProvider
@@ -19,11 +19,32 @@ animateApp.config(function($routeProvider) {
 
 animateApp.controller('mainController', function($scope) {
     $scope.pageClass = 'page-home';
+
 });
 
 animateApp.controller('feelingsController', function($scope) {
     $scope.pageClass = 'page-feelings';
-
+        var self = this;
+        self.todoList = [
+            {task: "happy", done: false},
+            {task: "to be here", done: false},
+            {task: "with you guys! ", done: false} ];
+        self.text; //bound to input box for new todo
+        self.addTodo = addTodo;
+        self.deleteTodo = deleteTodo;
+        function addTodo(){
+            // var newTodo = {task: self.text, done: false};
+            // self.todoList.push(newTodo);
+            self.todoList.push({task: self.text, done: false});
+            self.text = null;
+        }
+        function todone($index){
+            console.log(self.todoList);
+            //self.todoList.push({done: true});
+        }
+        function deleteTodo($index){
+            self.todoList.splice($index, 1);
+        }
 });
 
 animateApp.controller('breathController', function($scope) {
